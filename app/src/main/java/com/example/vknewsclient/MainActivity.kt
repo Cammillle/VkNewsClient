754ca02672597ca3792b560f59c01d203b754b8e
k.mordvinova <mordvinovacamilla@gmail.com>
2025-07-30 11:32:19 +0300

    ColumnScope

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -3,18 +3,17 @@ package com.example.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.vknewsclient.ui.theme.PostCard
 import com.example.vknewsclient.ui.theme.VkNewsClientTheme
@@ -23,17 +22,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VkNewsClientTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.background)
-                        .padding(10.dp)
-                ) {
-                    PostCard()
-                }
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                TestText(text = "text", count = 5)
             }
+
+
+//            VkNewsClientTheme {
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(MaterialTheme.colorScheme.background)
+//                        .padding(10.dp)
+//                ) {
+//                    PostCard()
+//                }
+//            }
         }
+    }
+}
+
+@Composable
+private fun ColumnScope.TestText(
+    text: String,
+    count: Int
+) {
+    repeat(count) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = text
+        )
     }
 }
 
