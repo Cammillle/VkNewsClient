61cdd2fa508c92183fcb745e9d8dd01a27e953e4
k.mordvinova <mordvinovacamilla@gmail.com>
2025-07-30 12:41:14 +0300

    Scaffold and NavigationDrawer

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -9,11 +9,31 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.vknewsclient.ui.theme.PostCard
 import com.example.vknewsclient.ui.theme.VkNewsClientTheme
@@ -22,12 +42,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                TestText(text = "text", count = 5)
-            }
-
+            ScaffoldTest()
+            NavigationDrawerTest()
 
 //            VkNewsClientTheme {
 //                Box(
@@ -43,16 +59,81 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+private fun ScaffoldTest() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                title = { Text(text = "TopAppBar title") },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Menu, contentDescription = null)
+                    }
+                })
+        },
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    selected = true,
+                    onClick = { },
+                    icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                    label = { Text("Favourite") }
+                )
+                NavigationBarItem(
+                    selected = true,
+                    onClick = { },
+                    icon = { Icon(Icons.Filled.Edit, contentDescription = null) },
+                    label = { Text("Edit") }
+                )
+                NavigationBarItem(
+                    selected = true,
+                    onClick = { },
+                    icon = { Icon(Icons.Filled.Delete, contentDescription = null) },
+                    label = { Text("Delete") }
+                )
+            }
+        }
+    ) {
+        Text(modifier = Modifier.padding(it), text = "This is scaffold content")
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun NavigationDrawerTest() {
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Text("Drawer title", modifier = Modifier.padding(16.dp))
+                HorizontalDivider()
+                NavigationDrawerItem(
+                    label = { Text("Drawer item") },
+                    icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                    selected = false,
+                    onClick = {}
+                )
+            }
+        }
+    ) { }
+}
+
+
 @Composable
 private fun ColumnScope.TestText(
-    text: String,
-    count: Int
+    text: String, count: Int
 ) {
     repeat(count) {
         Text(
-            modifier = Modifier.weight(1f),
-            text = text
+            modifier = Modifier.weight(1f), text = text
         )
     }
 }
+
+
 
