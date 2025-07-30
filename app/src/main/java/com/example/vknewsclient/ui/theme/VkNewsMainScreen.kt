06b04472a04a33d0e3548ece373a0ea27500eac8
k.mordvinova <mordvinovacamilla@gmail.com>
2025-07-30 14:14:41 +0300

    FAB and SnackBar

--- app/src/main/java/com/example/vknewsclient/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/example/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -4,28 +4,65 @@ import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
 @Composable
 fun MainScreen() {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
-        bottomBar = {
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState
+            )
+        }, floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            val action = snackbarHostState.showSnackbar(
+                                message = "This is snackbar",
+                                actionLabel = "Hide FAB",
+                                duration = SnackbarDuration.Long
+                            )
+                            if (action == SnackbarResult.ActionPerformed) {
+                                fabIsVisible.value = false
+                            }
+                        }
+                    }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        }, bottomBar = {
             NavigationBar {
-                Log.d("TEST","NavigationBar")
+                Log.d("TEST", "NavigationBar")
 
                 val selectedItemPosition = remember {
                     mutableIntStateOf(0)
@@ -36,9 +73,13 @@ fun MainScreen() {
                 items.forEachIndexed { index, item ->
                     NavigationBarItem(
                         selected = selectedItemPosition.intValue == index,
-                        onClick = {selectedItemPosition.intValue = index},
+                        onClick = { selectedItemPosition.intValue = index },
                         icon = { Icon(item.icon, contentDescription = null) },
-                        label = { Text(text = stringResource(item.titleResId)) }
+                        label = { Text(text = stringResource(item.titleResId)) },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.onBackground,
+                            selectedTextColor = MaterialTheme.colorScheme.onBackground
+                        )
                     )
                 }
             }
