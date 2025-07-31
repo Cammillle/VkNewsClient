9ce7618a3e42391005854d81510967932ef2a43b
k.mordvinova <mordvinovacamilla@gmail.com>
2025-07-31 17:49:03 +0300

    Jetpack Compose Navigation start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ android {
 dependencies {
     implementation(libs.androidx.runtime.livedata)
 
+    //Jetpack Compose Navigation
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/com/example/vknewsclient/navigation/AppNavGraph.kt
@@ -0,0 +1,18 @@
+package com.example.vknewsclient.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun AppNavGraph(
+    navHostController: NavHostController
+){
+    NavHost(
+        navController = navHostController,
+        startDestination = ,
+        graph = TODO(),
+    ){
+
+    }
+}
