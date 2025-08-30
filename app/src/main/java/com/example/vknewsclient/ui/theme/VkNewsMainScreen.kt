e4b684274dfb755b760cdfba8758f9130d4f6e8a
camilla <mordvinovacamilla@gmail.com>
2025-08-30 19:33:50 +0300

    navigation bag fix

--- app/src/main/java/com/example/vknewsclient/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/example/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -1,5 +1,8 @@
 package com.example.vknewsclient.ui.theme
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -9,11 +12,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.vknewsclient.MainViewModel
 import com.example.vknewsclient.navigation.AppNavGraph
+import com.example.vknewsclient.navigation.Screen
 
 @Composable
 fun MainScreen(
@@ -34,7 +46,15 @@ fun MainScreen(
                 items.forEach { item ->
                     NavigationBarItem(
                         selected = currentRoute == item.screen.route,
-                        onClick = { navHostController.navigate(item.screen.route) },
+                        onClick = {
+                            navHostController.navigate(item.screen.route) {
+                                popUpTo(Screen.NewsFeed.route) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
                         icon = { Icon(item.icon, contentDescription = null) },
                         label = { Text(text = stringResource(item.titleResId)) },
                         colors = NavigationBarItemDefaults.colors(
@@ -54,10 +74,28 @@ fun MainScreen(
                     paddingValues = paddingValues
                 )
             },
-            favouriteScreenContent = { Text("Favourite") },
-            profileScreenContent = { Text("Profile") }
+            favouriteScreenContent = {
+                TextCounter("Favourite", paddingValues)
+            },
+            profileScreenContent = {
+                TextCounter("Profile", paddingValues)
+            }
         )
-
-
     }
+}
+
+@Composable
+private fun TextCounter(name: String, paddingValues: PaddingValues) {
+    var count by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+
+    Text(
+        modifier = Modifier
+            .padding(paddingValues)
+            .padding(start = 8.dp)
+            .clickable { count++ },
+        text = "$name Count: $count",
+        color = Color.Black
+    )
 }
