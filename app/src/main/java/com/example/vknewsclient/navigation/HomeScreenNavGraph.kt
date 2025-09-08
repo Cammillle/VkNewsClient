1f1fdee50e86551c22cac546797d45b8811a1ef8
k.mordvinova <mordvinovacamilla@gmail.com>
2025-09-08 12:13:46 +0300

    fix arguments.getInt()

--- app/src/main/java/com/example/vknewsclient/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/com/example/vknewsclient/navigation/HomeScreenNavGraph.kt
@@ -1,23 +1,34 @@
 package com.example.vknewsclient.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavArgument
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import androidx.navigation.navArgument
 import com.example.vknewsclient.domain.FeedPost
 
+//Вложенный граф навигации
 fun NavGraphBuilder.homeScreenNavGraph(
     newsFeedScreenContent: @Composable () -> Unit,
     commentsScreenContent: @Composable (FeedPost) -> Unit
 ) {
-    navigation(  //Вложенный граф навигации
+    navigation(
         startDestination = Screen.NewsFeed.route,
         route = Screen.Home.route
     ) {
         composable(Screen.NewsFeed.route) {
             newsFeedScreenContent()
         }
-        composable(Screen.Comments.route) { //comments/{feed_post_id}
+        composable(
+            route = Screen.Comments.route,
+            arguments = listOf(
+                navArgument(Screen.KEY_FEED_POST_ID) {
+                    type = NavType.IntType
+                }
+            )
+        ) {                   //comments/{feed_post_id}
             val feedPostId = it.arguments?.getInt(Screen.KEY_FEED_POST_ID) ?: 0
             commentsScreenContent(FeedPost(id = feedPostId))
         }
