9647655bfa55bc856198d1b87a4d2b6120afea19
k.mordvinova <mordvinovacamilla@gmail.com>
2025-09-12 16:14:54 +0300

    vk sdk

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -5,8 +5,12 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.SideEffect
 import com.example.vknewsclient.ui.theme.MainScreen
 import com.example.vknewsclient.ui.theme.VkNewsClientTheme
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 
 class MainActivity : ComponentActivity() {
@@ -14,20 +18,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
-//                val launcher = rememberLauncherForActivityResult(
-//                    contract = VK.getVKAuthActivityResultContract(),
-//                    onResult = { result ->
-//                        when (result) {
-//                            is VKAuthenticationResult.Success -> {
-//                                Log.d("MYLOG", "Login success")
-//                            }
-//
-//                            is VKAuthenticationResult.Failed -> {
-//                                Log.d("MYLOG", "Login failed")
-//                            }
-//                        }
-//                    })
-//                launcher.launch(listOf(VKScope.WALL))
+                val launcher = rememberLauncherForActivityResult(
+                    contract = VK.getVKAuthActivityResultContract(),
+                    onResult = { result ->
+                        when (result) {
+                            is VKAuthenticationResult.Success -> {
+                                Log.d("MYLOG", "Login success")
+                            }
+
+                            is VKAuthenticationResult.Failed -> {
+                                Log.d("MYLOG", "Login failed")
+                            }
+                        }
+                    })
+                SideEffect {
+                    launcher.launch(listOf(VKScope.WALL))
+                }
                 MainScreen()
             }
         }
